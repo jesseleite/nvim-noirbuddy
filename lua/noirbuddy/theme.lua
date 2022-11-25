0ecbfe91c1b145f7b43a0b91121f932dc2ae3b6e
Jesse Leite <jesseleite@gmail.com>
2022-11-25 09:18:23 -0500

    Match to background color instead of black.

--- lua/noirbuddy/theme.lua
+++ lua/noirbuddy/theme.lua
@@ -8,7 +8,7 @@ Group.new('Search', colors.black, colors.white)
 Group.new('IncSearch', colors.black, colors.white)
 Group.new('Visual', nil, colors.gray_8)
 Group.new('SignColumn', nil, colors.black)
-Group.new('LineNr', colors.gray_8, colors.black)
+Group.new('LineNr', colors.gray_8, colors.bgcolor)
 
 Group.new('Comment', colors.gray_7)
 Group.new('Constant', colors.primary)
