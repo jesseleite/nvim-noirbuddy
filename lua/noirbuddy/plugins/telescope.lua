e2499fde716f3e850ba8761560d4998f195f155d
Dimitar Dimitrov <dimitrov2k@gmail.com>
2022-12-12 18:23:19 +0200

    minor code formatting fixes from previous commit

--- lua/noirbuddy/plugins/telescope.lua
+++ lua/noirbuddy/plugins/telescope.lua
@@ -1,8 +1,8 @@
 local Group = require('colorbuddy').Group
 local colors = require('colorbuddy').colors
 
-  -- Telescope colors
-  Group.new('TelescopeTitle', colors.gray_2)
+-- Telescope colors
+Group.new('TelescopeTitle', colors.gray_2)
 Group.new('TelescopeBorder', colors.gray_7)
 Group.new('TelescopeResultsNormal', colors.gray_5)
 Group.new('TelescopeMatching', colors.white)
