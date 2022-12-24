a40a787b8085dbf2bee5103c2245f90cb3299b6d
Jesse Leite <jesseleite@gmail.com>
2022-12-24 14:24:22 -0500

    Add `christmas` light theme preset 🎄

---
+++ lua/noirbuddy/presets/christmas.lua
@@ -0,0 +1,23 @@
+-- Merry Christmas!
+return {
+  background = '#ffffff',
+  primary = '#EC0034',
+  secondary = '#008E0E',
+  noir_0 = '#0C3011',
+  noir_1 = '#113616',
+  noir_2 = '#184B1E',
+  noir_3 = '#206428',
+  noir_4 = '#34743B',
+  noir_5 = '#44834B',
+  noir_6 = '#4E8F55',
+  noir_7 = '#89B38E',
+  noir_8 = '#C8DCCB',
+  noir_9 = '#E8F2E9',
+  diagnostic_error = '#EC0034',
+  diagnostic_warning = '#EEB913',
+  diagnostic_info = '#89B38E',
+  diagnostic_hint = '#89B38E',
+  diff_add = '#008E0E',
+  diff_change = '#89B38E',
+  diff_delete = '#EC0034',
+}
