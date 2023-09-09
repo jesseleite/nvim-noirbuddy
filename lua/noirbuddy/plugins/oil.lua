831797d4dd563fc1584b6fd986a74c8dc0f4565e
Jesse Leite <jesseleite@gmail.com>
2023-09-09 16:41:56 -0400

    Flesh out oil.nvim highlighting a bit more.

--- lua/noirbuddy/plugins/oil.lua
+++ lua/noirbuddy/plugins/oil.lua
@@ -2,4 +2,10 @@ local Group = require('colorbuddy').Group
 local colors = require('colorbuddy').colors
 
 Group.new('OilDir', colors.white)
+Group.new('OilFile', colors.noir_4)
 Group.new('OilLink', colors.primary)
+Group.new('OilCopy', colors.diff_add)
+Group.new('OilCreate', colors.diff_add)
+Group.new('OilChange', colors.diff_change)
+Group.new('OilMove', colors.diff_change)
+Group.new('OilDelete', colors.diff_delete)
