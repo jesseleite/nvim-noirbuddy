95e282ea7f4e5f060896eff0d312cd11136e64fb
Jesse Leite <jesseleite@gmail.com>
2023-11-20 02:56:39 -0500

    Fix issue with `setup()` getting called twice, as well as lualine’s color issue.
    When using a custom preset like `miami-nights`, lualine was coloring using the default preset because `setup()` was being run prematurely by colorbuddy, causing plugin modules like lualine to load prematurely with default color palette 🤯 …Future me will definitely not understand this lol!

--- lua/noirbuddy/init.lua
+++ lua/noirbuddy/init.lua
@@ -3,14 +3,15 @@ local M = {}
 function M.setup(opts)
   opts = opts or {}
 
-  require('colorbuddy').colorscheme('noirbuddy', false, {
-    disable_defaults = true,
-  })
+  vim.api.nvim_command("set termguicolors")
+  vim.api.nvim_command(string.format("set background=%s", 'dark'))
 
   require('noirbuddy.colors').setup(opts)
   require('noirbuddy.theme').setup(opts)
   require('noirbuddy.plugins')
   require('noirbuddy.languages')
+
+  vim.api.nvim_command(string.format('let g:colors_name = "%s"', 'noirbuddy'))
 end
 
 return M
