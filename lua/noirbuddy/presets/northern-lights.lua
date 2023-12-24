684c3aebd5d2fa1954188f439a702c2599f26ec2
theoboldalex <theoboldalex@gmail.com>
2023-12-24 21:47:11 +0000

    feat: adds nord inspired minimal theme

---
+++ lua/noirbuddy/presets/northern-lights.lua
@@ -0,0 +1,12 @@
+return require('noirbuddy.colors').mergeWithPreset('minimal', {
+    background = '#131313',
+    primary = '#A3BE8C',
+    secondary = '#8FBCBB',
+    diagnostic_error = '#BF616A',
+    diagnostic_warning = '#EBCB8B',
+    diagnostic_info = '#B48EAD',
+    diagnostic_hint = '#D08770',
+    diff_add = '#A3BE8C',
+    diff_change = '#EBCB8B',
+    diff_delete = '#BF616A'
+})
