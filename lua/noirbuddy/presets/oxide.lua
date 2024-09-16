5b69a979f891f32b1dc6d00062cbfa83ccef0070
Dimitar Dimitrov <dimitrov2k@gmail.com>
2024-09-16 19:37:54 +0300

    Add an Oxide Computers inspired preset

---
+++ lua/noirbuddy/presets/oxide.lua
@@ -0,0 +1,23 @@
+-- Inspired by oxide computers <3
+return {
+  background = "#070e11",
+  primary = "#00d992",
+  secondary = "#E9F5E9",
+  noir_0 = "#ffffff",
+  noir_1 = "#f5f5f5",
+  noir_2 = "#d5d5d5",
+  noir_3 = "#b4b4b4",
+  noir_4 = "#a7a7a7",
+  noir_5 = "#949494",
+  noir_6 = "#737373",
+  noir_7 = "#3A4649",
+  noir_8 = "#121b1d",
+  noir_9 = "#0c1214",
+  diagnostic_error = "#fa5e86",
+  diagnostic_warning = "#ffb717",
+  diagnostic_info = "#00d992",
+  diagnostic_hint = "#00d992",
+  diff_add = "#00d992",
+  diff_change = "#b4b4b4",
+  diff_delete = "#fa5e86",
+}
