9c09722e40d8d518a3d8d4fbe0dcbe99d0107906
Jesse Leite <jesseleite@gmail.com>
2024-09-21 13:31:09 -0400

    Show relative path with `filename` by default in lualine.

--- lua/noirbuddy/plugins/lualine.lua
+++ lua/noirbuddy/plugins/lualine.lua
@@ -87,7 +87,7 @@ M.theme = {
 M.sections = {
   lualine_a = { 'mode' },
   lualine_b = { 'branch', 'diff', { 'diagnostics', color = { bg = c.background } } },
-  lualine_c = { 'filename' },
+  lualine_c = { { 'filename', path = 1 } },
   lualine_x = { 'encoding', { 'filetype', colored = false } },
   lualine_y = { 'progress' },
   lualine_z = { 'location' }
@@ -96,7 +96,7 @@ M.sections = {
 M.inactive_sections = {
   lualine_a = {},
   lualine_b = {},
-  lualine_c = { 'filename' },
+  lualine_c = { { 'filename', path = 1 } },
   lualine_x = { 'location' },
   lualine_y = {},
   lualine_z = {}
