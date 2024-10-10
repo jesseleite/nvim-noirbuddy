bf55199ecd3c44ca184e2380d1278613d3454509
Jesse Leite <jesseleite@gmail.com>
2024-10-09 23:27:55 -0400

    Remove unintended bold styles.

--- lua/noirbuddy/plugins/mini-statusline.lua
+++ lua/noirbuddy/plugins/mini-statusline.lua
@@ -2,14 +2,13 @@ local cb = require('colorbuddy')
 
 local Group = cb.Group
 local colors = cb.colors
-local styles = cb.styles
 
 Group.new('MiniStatuslineModeNormal', colors.noir_2, colors.noir_7)
-Group.new('MiniStatuslineModeInsert', colors.background, colors.primary, styles.bold)
-Group.new('MiniStatuslineModeVisual', colors.background, colors.secondary, styles.bold)
-Group.new('MiniStatuslineModeReplace', colors.background, colors.primary, styles.bold)
-Group.new('MiniStatuslineModeCommand', colors.noir_2, colors.noir_7, styles.bold)
-Group.new('MiniStatuslineModeOther', colors.noir_2, colors.noir_7, styles.bold)
+Group.new('MiniStatuslineModeInsert', colors.background, colors.primary)
+Group.new('MiniStatuslineModeVisual', colors.background, colors.secondary)
+Group.new('MiniStatuslineModeReplace', colors.background, colors.primary)
+Group.new('MiniStatuslineModeCommand', colors.noir_2, colors.noir_7)
+Group.new('MiniStatuslineModeOther', colors.noir_2, colors.noir_7)
 
 Group.new('MiniStatuslineDevinfo', colors.noir_3, colors.noir_8)
 Group.new('MiniStatuslineFilename', colors.noir_3, colors.noir_9)
