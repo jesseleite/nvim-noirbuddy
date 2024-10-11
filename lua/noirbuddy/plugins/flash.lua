07deb1bf9570ae8e2ee5a05d22e08bd27e079d86
Jesse Leite <jesseleite@gmail.com>
2024-10-11 14:22:09 -0400

    Just use `NoirbuddyTextOnPrimary`, now that we can be sure text color will stay readable.

--- lua/noirbuddy/plugins/flash.lua
+++ lua/noirbuddy/plugins/flash.lua
@@ -1,17 +1,10 @@
 local Group = require('colorbuddy').Group
 local groups = require('colorbuddy').groups
-local colors = require('colorbuddy').colors
 
 Group.link('FlashBackdrop', groups.Comment)
 Group.link('FlashMatch', groups.Search)
 Group.link('FlashCurrent', groups.IncSearch)
+Group.link('FlashLabel', groups.NoirbuddyTextOnPrimary)
 -- Group.link('FlashPrompt', groups.MsgArea) -- TODO: Figure out where this is used?
 Group.link('FlashPromptIcon', groups.Special)
 Group.link('FlashCursor', groups.Cursor)
-
--- This one has been carefully balanced to stand out brighter against `Search` and `IncSearch` groups,
--- while not being too bright to avoid visually conflicting with flash's default f/F/t/T highlights.
--- Whether you're using flash's jump mode, `/` search with jump enabled, or f/F/t/T, you need
--- to be able to differentiate where the active cursor is against other matches. We also
--- want to avoid using primary & secondary bg colors against black & white fg text.
-Group.new('FlashLabel', colors.noir_0, colors.noir_7)
